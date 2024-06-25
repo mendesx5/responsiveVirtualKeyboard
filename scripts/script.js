ee2f9a121b6ae4e57c524dc8a9414038f9513863
mendesx5 <bielmendes2208@gmail.com>
2024-06-24 22:22:29 -0300

    third commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,43 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const keys = document.querySelectorAll('key');
+    const keys = document.querySelectorAll('.key');
     const output = document.getElementById('output');
+    const isCapsLockOn = false;
 
-    function addCharacter(character) {
+    function addCharacter (character) {
         output.value += character;
     }
+
+    function removeCharacter () {
+        output.value = output.value.slice(0, -1);
+    }
+
+    function clearAll () {
+        output.value = output.value.slice(0, -9999);
+    }
+    
+    function toggleCapsLock() {
+        isCapsLockOn = !isCapsLockOn;
+        keys.forEach(key => {
+            if (key.dataset.key && key.dataset.key.startsWith('Key')) {
+                key.textContent = isCapsLockOn ? key.textContent.toUpperCase() : key.textContent.toLowerCase();
+            }
+        });
+        document.getElementById('capslock').classList.toggle('active', isCapsLockOn);
+    }
+
+    keys.forEach(key => {
+        key.addEventListener('click', () => {
+            const character = key.textContent.trim();
+            if (key.id === 'key-space') {
+                addCharacter(' ')
+            } else if (key.id === 'key-backspace') {
+                removeCharacter()
+            } else if (key.id === 'clear-button') {
+                clearAll()
+            } else {
+                addCharacter(character)
+            }
+        })
+    })
 })
+
